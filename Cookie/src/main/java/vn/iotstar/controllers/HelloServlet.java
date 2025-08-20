1ae85200a1cc9d574683afc3502fce738b863fcf
NguyenThanhTin23 <thanhtin243@gmail.com>
2025-08-20 15:51:10 +0700

    Cookie

---
+++ Cookie/src/main/java/vn/iotstar/controllers/HelloServlet.java
@@ -0,0 +1,67 @@
+package vn.iotstar.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(urlPatterns = { "/hello", "/xin-chao" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter printWriter = resp.getWriter();
+		String name = "";
+
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			for (Cookie c : cookies) {
+				if (c.getName().equals("username")) {
+					name = c.getValue();
+					break;
+				}
+			}
+		}
+
+		if (name.equals("")) {
+			// chuyển sang trang LoginServlet
+			resp.sendRedirect(req.getContextPath() + "/login");
+			return;
+		}
+
+		// hiển thị lên trang bằng đối tượng PrintWriter()
+		printWriter.println("Xin chào " + name);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
